0f56db86d510cc6b1a6d47aee3a3abde0f6a0e2a
Srejic <srejic.ivan@hotmail.com>
2024-06-25 00:28:51 +0200

    initial commit

---
+++ src/main/java/com/example/zhbe/ZhBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhbe/ZhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
